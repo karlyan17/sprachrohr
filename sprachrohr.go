91f12e6fa7a8eb2da06438ba3fbfc9c8f5a03f71
karlyan17 <karlyan.kamerer@gmail.com>
2020-03-02 23:47:34 +0100

    implemented single item view and some boilerplate

--- sprachrohr.go
+++ sprachrohr.go
@@ -3,18 +3,20 @@ package main
 
 
 import(
-    //"fmt"
+    "github.com/karlyan17/jimbob"
     "github.com/gorilla/mux"
     "net/http"
     "io"
+    "strconv"
     "io/ioutil"
+    "html/template"
     //"sprachrohr/post"
     "sprachrohr/freshlog"
-    "sprachrohr/jimbob"
-    "html/template"
+    "sprachrohr/config"
 )
 
-var db  jimbob.Bucket
+var CONFIG config.Config
+var DB  jimbob.Bucket
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
     nBytes,err := io.WriteString(w, "main Hellow!")
@@ -24,41 +26,78 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func PostHandler(w http.ResponseWriter, r *http.Request) {
-    //req_post := ""
-    //for index,rpost := range(db.Data) {
-    //    req_post += fmt.Sprintf("%v:\tTitle: %v\tDate:%v\n%v\n", index, rpost.(post.Post)["Title"], rpost.(post.Post)["Created"], rpost.(post.Post)["Body"])
-    //}
+func PostHandler(writer http.ResponseWriter, request *http.Request) {
+    freshlog.Debug.Print("request is: ", request)
 
-    //nBytes,err := io.WriteString(w, fmt.Sprintf("post Hellow!\n%v", req_post))
-
-    templ_file,err := ioutil.ReadFile("posts.tmpl")
-    if err != nil {
-        freshlog.Error.Print("failed to read template: ", err)
+    switch request.Method {
+    case "GET":
+        PostView(writer, request)
+    case "POST":
+        PostPost(writer, request)
+    case "DELETE":
+        PostDelete(writer, request)
     }
 
-    templ := template.Must(template.New("posts").Parse(string(templ_file)))
+}
 
-    err = templ.Execute(w,db.Data)
+func PostView(writer http.ResponseWriter, request *http.Request) {
+    vars := mux.Vars(request)
+    freshlog.Debug.Print("vars are: ", vars)
+
+    if len(vars) == 0 {
+        serveTemplate("posts.tmpl", writer, DB.Data)
+    } else {
+        id,err := strconv.Atoi(vars["id"])
+        if err != nil {
+            serveTemplate("posts.tmpl", writer, DB.Data)
+        } else {
+            serveTemplate("post.tmpl", writer, DB.Data[id])
+        }
+    }
+}
+
+func PostPost(writer http.ResponseWriter, request *http.Request) {
+}
+
+func PostDelete(writer http.ResponseWriter, request *http.Request) {
+}
+
+func serveTemplate(tmpl_path string, writer http.ResponseWriter, data interface{}) {
+    templ_file,err := ioutil.ReadFile(tmpl_path)
+    if err != nil {
+        freshlog.Error.Print("failed to read template file: ", err)
+    }
+
+    templ,err := template.New(tmpl_path).Parse(string(templ_file))
+    if err != nil{
+        freshlog.Error.Print("failed to parse template: ", err)
+        return
+    }
+
+    err = templ.Execute(writer, data)
+
     if err != nil {
         freshlog.Error.Print("template error ", err)
+        return
     }
 }
 
 func main() {
     //
     freshlog.Debug.Print("opening jimbob bucket")
+    CONFIG = config.ParseFlags()
+    freshlog.Debug.Print(CONFIG)
     var err error
-    db,err = jimbob.OpenBucket("db")
+    DB,err = jimbob.OpenBucket(CONFIG.DB_path + "/posts")
     if err != nil {
-        freshlog.Fatal.Fatal("could not open jimbob db: ",err)
+        freshlog.Fatal.Fatal("could not open jimbob Bucket: ",err)
     }
 
     //for i := 0; i <10; i++ {
-    //    freshlog.Debug.Print("posting to db")
-    //    _,err = db.Post(apost)
+    //    freshlog.Debug.Print("posting to DB")
+    //    _,err = DB.Post(apost)
     //    if err != nil {
-    //        freshlog.Error.Print("could not POST to jimbob db: ",err)
+    //        freshlog.Error.Print("could not POST to jimbob DB: ",err)
     //    }
     //}
 
@@ -66,8 +105,8 @@ func main() {
     r := mux.NewRouter()
     r.HandleFunc("/", MainHandler)
     r.HandleFunc("/posts", PostHandler)
-    r.HandleFunc("/posts/{id}", PostHandler)
+    r.HandleFunc("/posts/{id:[0-9]*}", PostHandler)
 
     //do shit
-    freshlog.Fatal.Fatal(http.ListenAndServe(":8080", r))
+    freshlog.Fatal.Fatal(http.ListenAndServe(CONFIG.IP + ":" + strconv.Itoa(CONFIG.Port), r))
 }
